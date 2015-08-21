9360878f72401a05af80c3dd678145f6c708eec5
mulraj <mulrajgala@yahoo.com>
2015-08-21 00:22:40 -0400

    first version of assignment 2 for Coursera R course

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is programming assignment # 2 for "R programming" language course.
+## The file contains 2 functions as described in detail below
+## The purpose of assignment seems like to teach concept of 
+## function factory and caching.
 
-## Write a short comment describing this function
+## Function "makeCacheMatrix" defines and stores following functions:
+## setM - caches the supplied matrix and resets "inverseMatrix" to NULL
+##             as it needs to be recalculated and cached
+## getM - returns cached matrix
+## setInvM - caches the "inverse" matrix 
+## getInvM - returns "inversed"cached matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { 
+  invM <- NULL    ## initialize variable where inverse matrix will be stored
+  setM <- function(y) { ## this function stores the original matrix
+    x <<- y             ## permanantly store suppied matrix y in x using global assignment operator
+    invM <<- NULL       ## initialize variable where inverse matrix will be stored
+  }
+  getM <- function() x  ## get value of original matrix
+  setInvM <- function(invMatrix) invM <<- invMatrix ## set value of "inverse" matrix
+  getInvM <- function() invM ## get value of "inverse" matrix
+  list(setM = setM, getM = getM, 
+       setInvM = setInvM,
+       getInvM = getInvM)
 }
 
+## The function cacheSolve 
 
-## Write a short comment describing this function
+cacheSolve <- function(x, ...) { 
+  ## Return a matrix that is the inverse of 'x' 
+  invM <- x$getInvM()
+  if(!is.null(invM)) {
+    message("getting cached data")
+    return(invM)
+  }
+  data <- x$getM()
+  invM <- solve(data)
+  x$setInvM(invM)
+  invM
+} 
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
